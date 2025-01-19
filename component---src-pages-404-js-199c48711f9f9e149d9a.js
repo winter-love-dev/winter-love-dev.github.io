07ee518b0c4caeec22e6e6591339c83d0d5287d6
winter-love-dev <winter-love-dev@users.noreply.github.com>
2025-01-19 03:53:54 +0000

    deployed via Gatsby Publish Action 🎩 for ac83968f742d20bbdba08035f57a64c28f5b6465

---
+++ component---src-pages-404-js-199c48711f9f9e149d9a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwinter_love_dev=self.webpackChunkwinter_love_dev||[]).push([[125],{3331:function(e,t,n){n.r(t);var l=n(6540),u=n(6436),r=n(3493);t.default=function(){return l.createElement(u.A,null,l.createElement(r.A,{title:"404: Not found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-199c48711f9f9e149d9a.js.map
