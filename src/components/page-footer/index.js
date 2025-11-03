5b4dcb54843ad6ecb40c1bfdffa408106813cea5
winter-love-dev <hun230402@gmail.com>
2025-11-03 21:05:50 +0900

    Update footer text

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -5,7 +5,7 @@ function PageFooter({ author, githubUrl }) {
   return (
     <footer className="page-footer-wrapper">
       <p className="page-footer">
-        © 2025. Seonghun Kim all rights reserved.
+        © Winter archive 2025
       </p>
     </footer>
   );
